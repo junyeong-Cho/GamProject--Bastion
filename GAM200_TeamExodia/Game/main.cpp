50412922623f4bfe76f3a68718becbd23053270c
Junyeong-Cho <tpqldj1312@gmail.com>
2023-09-25 20:44:37 +0900

    Update properties for the github actions

--- GAM200_TeamExodia/Game/main.cpp
+++ GAM200_TeamExodia/Game/main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <doodle/doodle.hpp>
+#include <string>
 
-int main(int argc, char** argv)
+using namespace doodle;
+
+int main(void)
 {
-	std::cout << "Hello World!" << std::endl;
-	return 0;
+    create_window("Orange Window");
+
+    using namespace std::string_literals;
+
+    const std::string text = "("s + std::to_string(Width) + ","s + std::to_string(Height) + ")"s;
+
+
+    while (!is_window_closed())
+    {
+        update_window();
+        clear_background(HexColor{ 0xC75023FF });
+        draw_text(text, 0, 0);
+    }
+
+    return 0;
 }
+
