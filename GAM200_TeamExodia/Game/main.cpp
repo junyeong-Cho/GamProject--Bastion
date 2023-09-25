ffa0cd23a6a4859390788552340671e2d1a67b19
Junyeong-Cho <tpqldj1312@gmail.com>
2023-09-25 22:20:36 +0900

    Revert "Update properties for the github actions"
    This reverts commit 50412922623f4bfe76f3a68718becbd23053270c.

--- GAM200_TeamExodia/Game/main.cpp
+++ GAM200_TeamExodia/Game/main.cpp
@@ -1,24 +1,7 @@
-#include <doodle/doodle.hpp>
-#include <string>
+#include <iostream>
 
-using namespace doodle;
-
-int main(void)
+int main(int argc, char** argv)
 {
-    create_window("Orange Window");
-
-    using namespace std::string_literals;
-
-    const std::string text = "("s + std::to_string(Width) + ","s + std::to_string(Height) + ")"s;
-
-
-    while (!is_window_closed())
-    {
-        update_window();
-        clear_background(HexColor{ 0xC75023FF });
-        draw_text(text, 0, 0);
-    }
-
-    return 0;
+	std::cout << "Hello World!" << std::endl;
+	return 0;
 }
-
