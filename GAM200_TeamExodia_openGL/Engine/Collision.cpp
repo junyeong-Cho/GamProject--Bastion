b003b2333e1b15cb94faaae15b8782b7c6f0eae9
Junyeong-Cho <tpqldj1312@gmail.com>
2023-11-11 17:51:41 +0900

    collision fixed

--- GAM200_TeamExodia_openGL/Engine/Collision.cpp
+++ GAM200_TeamExodia_openGL/Engine/Collision.cpp
@@ -86,15 +86,13 @@ Math::rect GAM200::RectCollision::WorldBoundary()
 void GAM200::RectCollision::Draw(Math::TransformationMatrix display_matrix)
 {
     DrawShape draw_shape;
-    draw_shape.SetColor(1.0f, 1.0f, 1.0f, 1.0f);  // White color
-    draw_shape.SetLineWidth(50);
+    //draw_shape.SetColor(1.0f, 1.0f, 1.0f, 1.0f);  // White color
+    //draw_shape.SetLineWidth(50);
 
-    
     Math::rect world_boundary = WorldBoundary();
-    glPushMatrix();
-    glMultMatrixd(&(display_matrix[0][0]));
-    draw_shape.DrawRectangle((int)world_boundary.Left(), (int)world_boundary.Bottom(), (int)world_boundary.Size().x, (int)world_boundary.Size().y);
-    glPopMatrix();
+
+
+    draw_shape.DrawRectangle_NoFill_WithBoundaries(world_boundary, display_matrix);
     
 }
 
