88fcf6d079c2157e736136fea3a3a82e6da46869
HyeonjoonNam-DigiPen <guswns409@naver.com>
2023-11-14 19:11:39 +0900

    GamePlayEditor Scene

--- GAM200_TeamExodia_openGL/Game/GamePlayEditor.h
+++ GAM200_TeamExodia_openGL/Game/GamePlayEditor.h
@@ -1,47 +1,76 @@
+
 /*
 Copyright (C) 2023 DigiPen Institute of Technology
 Reproduction or distribution of this file or its contents without
 prior written consent is prohibited
-File Name:  GamePlayEditor.h
-Project:    GAM200 Engine
-Author:     
-Created:    November 13, 2023
-Updated:    November 13, 2023
+File Name:  Player.h
+Project:    GAM200_TeamExodia_openGL
+Author:     Hyeonjoon Nam
+Created:    October		10, 2023
+Updated:    October		10, 2023
 */
 
 #pragma once
 
-#include "../Game/States.h"
-#include "../Game/Background.h"
+#include "IProgram.h"
 
 #include "../Engine/GameObjectManager.h"
 #include "../Engine/TextureManager.h"
-
-#include "../Engine/Texture.h"
-#include "../Engine/Font.h"
-
+#include "../Engine/Camera.h"
 #include "../Engine/GameState.h"
-#include "../Engine/Sprite.h"
+#include "../Engine/Texture.h"
+#include "GameObjectTypes.h"
+#include "../Game/Hp_Boost_Gold_ui.h"
+
+class Player;
 
 
 class GamePlayEditor : public GAM200::GameState
 {
 public:
-	GamePlayEditor();
+    GamePlayEditor();
+    void Load()                                  override;
+    void Update(double dt)                       override;
+    void Unload()                                override;
+    void Draw()                                  override;
 
-	void Load()				override;
-	void Update(double dt)  override;
-	void Unload()	        override;
-	void Draw()				override;
+    void ImguiDraw()                             override;
+    void HandleEvent(SDL_Event& event)           override;
 
-	void ImguiDraw() override;
-	void HandleEvent(SDL_Event& event) override;
+    void SetMap(std::string file_name);
 
 
-	std::string GetName() override { return "GamePlayEditor"; }
+    std::string GetName() override { return "Mode1"; }
+public:
 
+    double counter = 0;
+
+    int x = 0;
+    int y = 0;
+    int velocity = 500;
+
+    Player* player_ptr;
+
+
+    int tile_col = 0;
+    int tile_row = 0;
+    Math::ivec2 tile_size;
+
+    enum  Button_Type
+    {
+        basic_tower,
+        double_tower,
+        triple_tower,
+        delete_tower,
+        change_t_p,
+        change_t_b
+
+
+
+    };
 
 private:
-	std::unique_ptr<GAM200::Texture> message;
 
+    GAM200::Texture m = GAM200::Texture("assets/images/m.png", GAM200::Texture::TextureType::RECTANGLE);
+    GAM200::Texture w = GAM200::Texture("assets/images/w.png", GAM200::Texture::TextureType::RECTANGLE);
 };
