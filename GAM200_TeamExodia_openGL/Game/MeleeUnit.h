4d0886fa1841c279837293365fbb3cb42522c1ee
HyeonjoonNam-DigiPen <guswns409@naver.com>
2024-03-27 17:08:24 +0900

    Draw functions simplify

--- GAM200_TeamExodia_openGL/Game/MeleeUnit.h
+++ GAM200_TeamExodia_openGL/Game/MeleeUnit.h
@@ -63,15 +63,11 @@ public:
 
     void Draw(Math::TransformationMatrix camera_matrix)
     {
-        Math::vec2 position = GetPosition();
-        GAM200::DrawShape shape;
+        Unit::Draw(camera_matrix);
 
-        
-        Engine::Instance().push();
-        shape.SetColor(0.0f, 0.0f, 0.0f, 0.2f);//타워 발판 표시
-        shape.DrawCircle(static_cast<int>(position.x), static_cast<int>(position.y), static_cast<int>(radius), static_cast<int>(radius));
-        Engine::Instance().pop();
-        
+        Math::vec2 position = GetPosition();
+
+        // Unit draw        
         if(current_state->GetName() == "None")
         {
             melee_attack->Draw(static_cast<int>(position.x) - 85/2, static_cast<int>(position.y), 170/2, 185/2);
@@ -80,16 +76,6 @@ public:
         {
             melee_idle->Draw(static_cast<int>(position.x) - 85/2, static_cast<int>(position.y), 170/2, 185/2);
         }
-
-        
-        if (is_moving)
-        {
-            Engine::Instance().push();
-            shape.SetColor(0.977f, 0.157f, 0.569f, 0.3f);//범위 표시
-            shape.DrawCircle(static_cast<int>(position.x), static_cast<int>(position.y), static_cast<int>(range), static_cast<int>(range));
-            Engine::Instance().pop();
-        }
-        
      }
 
 
